cf941d9e7527aaa6fb93abe96ceb7a029e7a8fac
iikim <iikim2511@gmail.com>
2020-12-03 11:52:09 -0500

    1203-2

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+git 
+// ● C 02 자바스크립트의 목적과 셀렉터
+document.write("● C 02 자바스크립트의 목적과 셀렉터");
+console.log("● C 02 자바스크립트의 목적과 셀렉터");
+
+//  C 58 . JS 58
+
+// [Selectors]
+
+// [Event Listeners]
+
+// [Funcitons]
+function name(event) {
+  // todo DIV
+  // Create LI
+}
